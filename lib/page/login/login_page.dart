d6e2ed4b1bb0fb2f828b9b22d394ccb5c65c14d6
amirhoosian <amirhoosian78@gmail.com>
2024-10-13 12:20:37 +0330

    start build app

---
+++ lib/page/login/login_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: const Center(
+        child: Text(
+          'LoginPage is working',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
